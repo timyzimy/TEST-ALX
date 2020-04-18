acd6096de5662597b26b64cc2d4491d49366c416
timyzimy <63906270+timyzimy@users.noreply.github.com>
2020-04-18 15:40:48 +0200

    Update Okrag.cs

--- Dziedziczenie/Figury/Okrag.cs
+++ Dziedziczenie/Figury/Okrag.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+To jest modyfikacja wykonana z poziomu strony GitHub
+*/
 namespace Dziedziczenie.Figury
 {
     class Okrag
