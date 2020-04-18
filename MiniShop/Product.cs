c50bfaba68261b978b99ad34063bb4540cc4a79e
TimyZimy <hubert.sobieszczyk@gmail.com>
2020-04-18 17:05:47 +0200

    commit na koniec dnia

---
+++ MiniShop/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    public class Product
+    {
+        private int id;
+        private String name;
+        private double price;
+        private String descr = String.Empty;
+        private int version = 0;
+
+        private void SetParams(int id, String name, double price, String descr = "")
+        { 
+        }
+
+    }
+}
