d51a28bea5a50ddcaa33000d112f190c34fcc451
TimyZimy <hubert.sobieszczyk@gmail.com>
2020-04-19 12:39:53 +0200

    Final changes

---
+++ MetodyWirtualne/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodyWirtualne
+{
+    class Ssak
+    {
+        public void Karmienie()
+        {
+            Console.WriteLine("Operacja karmienia");
+        }
+    }
+
+    class Kot : Ssak
+    {
+        public new void Karmienie()
+        {
+            Console.WriteLine("Kot - karmienie");
+        }
+
+        public void DajGlos()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Obiekt klasy Kot";
+        }
+    }
+}
---
+++ MetodyWirtualne/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodyWirtualne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String napis = "Ala ma kota";
+            Console.WriteLine(napis);
+
+            Double liczba = 12.34;
+            Console.WriteLine(liczba);
+
+            Kot kot = new Kot();
+            Console.WriteLine(kot);
+
+            Console.ReadKey();
+        }
+    }
+}
